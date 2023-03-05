bfddd79c018bf4e5b66ad663c3f7804c56212e03
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2023-03-06 00:40:16 +0100

    Picking a random element

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    setCount(count + 1);
+    // animals.push(getRandomAnimal())  - modifies a piece of state!!!
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
 
-      <div>
-        Number of animals: {count}
-      </div>
+      <div>{animals}</div>
     </div>
   );
 }
